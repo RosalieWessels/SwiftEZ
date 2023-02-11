361ac2150bbc980a2e69a98a8d8ae239cc9deb62
RosalieWessels <rosalie.wessels@yahoo.com>
2023-02-11 09:07:02 -0800

    Initial Commit

---
+++ Shared/SwiftEZApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftEZApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftEZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
