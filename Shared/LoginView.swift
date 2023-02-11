73b2e63e278c04ad252102d66a59b52b672d8b61
RosalieWessels <rosalie.wessels@yahoo.com>
2023-02-11 10:12:21 -0800

    added Firebase

---
+++ Shared/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SwiftEZ
+//
+//  Created by Rosalie Wessels on 2/11/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
